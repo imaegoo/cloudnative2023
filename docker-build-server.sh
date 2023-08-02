ad8743c73f31824925e70aba2103fff2f7d22dde
iMaeGoo <mail1st@qq.com>
2023-08-02 20:32:22 +0800

    fix: cleanup

--- docker-build-server.sh
+++ docker-build-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eux
-docker build -f Dockerfile.server -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.6 .
-docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.6
+docker build -f Dockerfile.server -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.7 .
+docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.7
--- docker-build-web.sh
+++ docker-build-web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eux
-docker build -f Dockerfile.web -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.6 .
-docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.6
+docker build -f Dockerfile.web -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.7 .
+docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.7
