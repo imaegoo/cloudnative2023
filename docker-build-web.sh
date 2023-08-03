f2d728b004e5b18955350aaf5f30e247427642d9
iMaeGoo <mail1st@qq.com>
2023-08-03 11:26:07 +0800

    chore: bump version

--- docker-build-web.sh
+++ docker-build-web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eux
-docker build -f Dockerfile.web -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.9 .
-docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.9
+docker build -f Dockerfile.web -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.10 .
+docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023:0.1.10
