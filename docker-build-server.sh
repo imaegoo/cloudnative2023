d4e83c196d32512371092e965d88ae8af6b2932f
iMaeGoo <mail1st@qq.com>
2023-08-03 09:41:55 +0800

    fix: cors allowedOriginPatterns

--- docker-build-server.sh
+++ docker-build-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -eux
-docker build -f Dockerfile.server -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.8 .
-docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.8
+docker build -f Dockerfile.server -t registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.9 .
+docker push registry.cn-hangzhou.aliyuncs.com/imaegoo/cloudnative2023server:0.1.9
